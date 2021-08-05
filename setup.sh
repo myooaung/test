f0ae25467dcf6ec0effca832e85e025c82aa7390
Myo Aung <57899704+myooaung@users.noreply.github.com>
2021-08-05 12:41:37 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 apt-get install curl
 curl https://rclone.org/install.sh | sudo bash
 curl https://git.io/gclone.sh | sudo bash
+mkdir ./.config
 mkdir ./.config/rclone
 cd ./.config/rclone/
 touch rclone.conf
