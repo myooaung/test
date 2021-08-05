db66221bdd8d3b73e8582e75bbfa209525c7f065
Myo Aung <57899704+myooaung@users.noreply.github.com>
2021-08-05 12:04:13 +0630

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo bash <(wget -qO- https://rclone.org/install.sh)
+sudo bash <(wget -qO- https://git.io/gclone.sh)
+mkdrir ./.config/rclone
+cd ./.config/rclone/
+touch rclone.conf
+nano rclone.conf
